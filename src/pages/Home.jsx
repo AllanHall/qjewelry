3d70efc58dba13bce533d60133e02353e8f146ba
Allan Hall <bigal76126@gmail.com>
2019-07-29 19:03:03 -0400

    finished barebones css and html of page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { slide as Menu } from 'react-burger-menu'
 import * as data from '../data/mockData.json'
 import jewel from '../images/jewel.jpg'
-import q from '../images/flaming-q.jpg'
 
 export default function Home() {
   return (
@@ -17,41 +16,44 @@ export default function Home() {
         <a className="menu-item" href="#spinner">
           Patriotic Spinner Flag Bracelet
         </a>
-        <a id="rabbit" className="menu-item" href="/rabbit">
+        <a className="menu-item" href="#rabbit">
           Down the Rabbit Hole
         </a>
-        <a id="pill" className="menu-item" href="/pill">
+        <a className="menu-item" href="#pill">
           Red Pill
         </a>
-        <a id="show" className="menu-item" href="/show">
+        <a className="menu-item" href="#show">
           Enjoy the Show!
         </a>
-        <a id="q" className="menu-item" href="/q">
+        <a className="menu-item" href="#q">
           Q and Q-anon
         </a>
-        <a id="move" className="menu-item" href="/move">
+        <a className="menu-item" href="#move">
           Q Movement
         </a>
-        <a id="gold" className="menu-item" href="/gold">
+        <a className="menu-item" href="#gold">
           Gold
         </a>
-        <a id="silver" className="menu-item" href="/silver">
+        <a className="menu-item" href="#silver">
           Silver
         </a>
-        <a id="rose" className="menu-item" href="/rose">
+        <a className="menu-item" href="#rose">
           Rose Gold
         </a>
-        <a id="metal" className="menu-item" href="/metal">
+        <a className="menu-item" href="#metal">
           Gunmetal
         </a>
-        <a id="snap" className="menu-item" href="/snap">
+        <a className="menu-item" href="#snap">
           Snap Jewelry
         </a>
-        <a id="more" className="menu-item" href="/more">
+        <a className="menu-item" href="#more">
           More
         </a>
+        <a className="menu-item" href="#top">
+          Top of Page
+        </a>
       </Menu>
-      <section className="header">
+      <section className="header" id="top">
         {/* <img src={q} /> */}
         <div className="main-title">Q-anon Jewelry</div>
         <div className="motto">A company by Patriots for Patriots!</div>
@@ -73,7 +75,7 @@ export default function Home() {
       <section className="trump" id="trump">
         <div className="page-header">Trump</div>
         <div className="body">
-          {data.swamp.map(piece => (
+          {data.trump.map(piece => (
             <div className="jewelry-cell">
               <img className="jewelry-photo" src={jewel} />
               <p>{piece.name}</p>
@@ -86,6 +88,163 @@ export default function Home() {
       </section>
       <section className="spinner" id="spinner">
         <div className="page-header">Patriotic Spinner Flag Bracelet</div>
+        <div className="body">
+          {data.spinner.map(piece => (
+            <div className="jewelry-cell spinner-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="rabbit" id="rabbit">
+        <div className="page-header">Down the Rabbit Hole</div>
+        <div className="body">
+          {data.rabbit.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="pill" id="pill">
+        <div className="page-header">Red Pill</div>
+        <div className="body">
+          {data.pill.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="show" id="show">
+        <div className="page-header">Enjoy the Show!</div>
+        <div className="body">
+          {data.show.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="q" id="q">
+        <div className="page-header">Q and Q-anon</div>
+        <div className="body">
+          {data.q.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="move" id="move">
+        <div className="page-header">Q Movement</div>
+        <div className="body">
+          {data.move.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="gold" id="gold">
+        <div className="page-header">Gold</div>
+        <div className="body">
+          {data.gold.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="silver" id="silver">
+        <div className="page-header">Silver</div>
+        <div className="body">
+          {data.silver.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="rose" id="rose">
+        <div className="page-header">Rose Gold</div>
+        <div className="body">
+          {data.rose.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="metal" id="metal">
+        <div className="page-header">Gunmetal</div>
+        <div className="body">
+          {data.metal.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="snap" id="snap">
+        <div className="page-header">Snap Jewelry</div>
+        <div className="body">
+          {data.snap.map(piece => (
+            <div className="jewelry-cell">
+              <img className="jewelry-photo" src={jewel} />
+              <p>{piece.name}</p>
+              <p>{piece.description}</p>
+              <p>{piece.precision}</p>
+              <p>$ {piece.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="footer" id="more">
+        <a className="top" href="#top">
+          Top of Page
+        </a>
+        <div className="copyright">Copyright Q-Jewelry 2019</div>
       </section>
     </div>
   )
